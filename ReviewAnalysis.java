cb8395d9011566ec046e9cc4a08a4eaacc76fdd3
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2025-04-08 16:34:17 +0000

    first

---
+++ ReviewAnalysis.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class ReviewAnalysis
+{
+    private Review[] allReviews;
+
+    public ReviewAnalysis(Review[] reviews){
+        allReviews = reviews;
+    }
+
+    public double getAverageRating(){
+        double sum = 0;
+        for (Review r: allReviews) sum += r.getRating();
+        return sum / allReviews.length;
+    }
+
+    ArrayList<String> comments = new ArrayList<String>();
+    public ArrayList<String> collectComments(){
+
+    }
+}
